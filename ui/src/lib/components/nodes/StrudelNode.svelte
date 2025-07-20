14f7d04479ed8f14a4f6136190a07a786427d50c
Phoomparin Mano <phoomparin@gmail.com>
2025-07-21 04:13:04 +0700

    remove redundant edit code button

--- ui/src/lib/components/nodes/StrudelNode.svelte
+++ ui/src/lib/components/nodes/StrudelNode.svelte
@@ -100,14 +100,6 @@
 							</button>
 						{/if}
 					{/if}
-
-					<button
-						class="rounded p-1 opacity-0 transition-opacity hover:bg-zinc-700 group-hover:opacity-100"
-						onclick={toggleEditor}
-						title="Edit code"
-					>
-						<Icon icon="lucide:code" class="h-4 w-4 text-zinc-300" />
-					</button>
 				</div>
 			</div>
 
