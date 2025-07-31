c7bf18fd62764b7f87f84a39a4272e28d6184781
Phoomparin Mano <phoomparin@gmail.com>
2025-08-01 06:51:39 +0700

    remove dev

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,16 +2,4 @@
 	import FlowCanvas from '$lib/components/FlowCanvas.svelte';
 </script>
 
-<div class="relative">
-	<FlowCanvas />
-	
-	<!-- Development links -->
-	<div class="fixed right-4 top-4 z-50">
-		<a 
-			href="/fbo-test" 
-			class="rounded bg-blue-600 px-3 py-2 text-sm text-white hover:bg-blue-700"
-		>
-			FBO Test
-		</a>
-	</div>
-</div>
+<FlowCanvas />
