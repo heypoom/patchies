70a786b955837259ec5cf70ad68bfc5f80ccc701
Phoomparin Mano <phoomparin@gmail.com>
2025-08-02 20:35:24 +0700

    hydra: remove use of fast in beans preset

--- ui/src/lib/presets/hydra.presets.ts
+++ ui/src/lib/presets/hydra.presets.ts
@@ -7,7 +7,6 @@ src(s0).diff(s1).out(o0)`;
 
 const BEANS = `osc(30,0.01,1)
   .mult(osc(20,-0.1,1).modulate(noise(3,1)).rotate(0.7))
-  .posterize([3,10,2].fast(0.5).smooth(1))
   .out(o0)`;
 
 export const HYDRA_PRESETS: Record<string, { type: string; data: { code: string } }> = {
