ac4560de23c433ddb354ae1ba9a01dfc3adc36a8
Phoomparin Mano <phoomparin@gmail.com>
2025-08-03 21:36:48 +0700

    preset: add frame-timer.js

--- ui/src/lib/presets/js.presets.ts
+++ ui/src/lib/presets/js.presets.ts
@@ -5,16 +5,24 @@ const DELAY_JS = `onMessage(async (m) => {
   send(m.data)
 })`;
 
+const FRAME_TIMER_JS = `setInterval(() => {
+  send({type: 'bang'})
+}, 1000 / 60)`;
+
 export const JS_PRESETS: Record<
 	string,
 	{ type: string; data: { code: string; showConsole?: boolean; runOnMount?: boolean } }
 > = {
-	'message_console.js': {
+	'message-console.js': {
 		type: 'js',
 		data: { code: MESSAGE_CONSOLE_JS.trim(), showConsole: true, runOnMount: true }
 	},
 	'delay.js': {
 		type: 'js',
 		data: { code: DELAY_JS.trim(), showConsole: false, runOnMount: true }
+	},
+	'frame-timer.js': {
+		type: 'js',
+		data: { code: FRAME_TIMER_JS, showConsole: false, runOnMount: false }
 	}
 };
