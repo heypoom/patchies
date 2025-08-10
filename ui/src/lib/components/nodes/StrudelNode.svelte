bcf158683761c7fced1f7486a8a1af2efd44818f
Phoomparin Mano <phoomparin@gmail.com>
2025-08-11 03:40:43 +0700

    strudel: change title color

--- ui/src/lib/components/nodes/StrudelNode.svelte
+++ ui/src/lib/components/nodes/StrudelNode.svelte
@@ -100,7 +100,7 @@
 		<div class="flex flex-col gap-2">
 			<div class="absolute -top-7 left-0 flex w-full items-center justify-between">
 				<div class="z-10 rounded-lg bg-zinc-900 px-2 py-1">
-					<div class="font-mono text-xs font-medium text-zinc-100">strudel</div>
+					<div class="font-mono text-xs font-medium text-zinc-400">strudel</div>
 				</div>
 
 				<div class="flex items-center gap-1">
