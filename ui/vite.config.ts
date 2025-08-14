7927c46ee7f36563dbc90fcd45cdcccc1084251f
Phoomparin Mano <phoomparin@gmail.com>
2025-08-15 02:26:51 +0700

    fix vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,9 +2,27 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const PYODIDE_EXCLUDE = ['!**/*.{md,html}', '!**/*.d.ts', '!**/*.whl', '!**/node_modules'];
+
+export function viteStaticCopyPyodide() {
+	const pyodideDir = dirname(fileURLToPath(import.meta.resolve('pyodide')));
+
+	return viteStaticCopy({
+		targets: [
+			{
+				src: [join(pyodideDir, '*')].concat(PYODIDE_EXCLUDE),
+				dest: 'assets'
+			}
+		]
+	});
+}
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), viteStaticCopyPyodide()],
 	optimizeDeps: { exclude: ['pyodide'] },
 	define: {
 		global: 'globalThis'
