433aae598a6c28acb3103503da89a9ae1f54885b
Phoomparin Mano <phoomparin@gmail.com>
2025-08-25 06:16:09 +0700

    make markdown header only visible on selected

--- ui/src/lib/components/nodes/MarkdownNode.svelte
+++ ui/src/lib/components/nodes/MarkdownNode.svelte
@@ -85,9 +85,11 @@
 <div class="relative">
 	<NodeResizer class="z-1" isVisible={props.selected} />
 
-	<div class="absolute -top-7 z-10 w-fit rounded-lg bg-zinc-900/60 px-2 py-1 backdrop-blur-lg">
-		<div class="font-mono text-xs font-medium text-zinc-400">markdown</div>
-	</div>
+	{#if props.selected}
+		<div class="absolute -top-7 z-10 w-fit rounded-lg bg-zinc-900/60 px-2 py-1 backdrop-blur-lg">
+			<div class="font-mono text-xs font-medium text-zinc-400">markdown</div>
+		</div>
+	{/if}
 
 	<div>
 		<Handle type="target" position={Position.Top} class={handleClass} />
