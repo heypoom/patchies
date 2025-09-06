dbbd84a44dce2cf141dcf2d225505a532f2109a8
Phoomparin Mano <phoomparin@gmail.com>
2025-09-07 02:38:16 +0700

    p5: make width and height depend on default canvas size

--- ui/src/lib/p5/constants.ts
+++ ui/src/lib/p5/constants.ts
@@ -1,10 +1,16 @@
+import { DEFAULT_OUTPUT_SIZE, PREVIEW_SCALE_FACTOR } from '$lib/canvas/constants';
+
+const defaultWidth = Math.round(DEFAULT_OUTPUT_SIZE[0] / PREVIEW_SCALE_FACTOR);
+
+const defaultHeight = Math.round(DEFAULT_OUTPUT_SIZE[1] / PREVIEW_SCALE_FACTOR);
+
 export const DEFAULT_P5_CODE = `function setup() {
-  createCanvas(200, 150)
-  pixelDensity(4)
+  createCanvas(${defaultWidth}, ${defaultHeight})
+  pixelDensity(${PREVIEW_SCALE_FACTOR})
 }
 
 function draw() {
   clear()
   fill(255, 255, 100)
-  ellipse(100, 75, 80, 80)
+  ellipse(${defaultWidth / 2}, ${defaultHeight / 2}, 80, 80)
 }`;
