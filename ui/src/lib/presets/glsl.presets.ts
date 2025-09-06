40243770dfd959b75c9efa126c4e6b9cf987b0e4
Phoomparin Mano <phoomparin@gmail.com>
2025-09-07 03:56:24 +0700

    preset: add switcher.gl

--- ui/src/lib/presets/glsl.presets.ts
+++ ui/src/lib/presets/glsl.presets.ts
@@ -64,6 +64,27 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   fragColor = vec4(0.1, wave, 1. - wave, 0.9);
 }`;
 
+const SWITCHER_GL = `uniform sampler2D a;
+uniform sampler2D b;
+uniform sampler2D c;
+uniform sampler2D d;
+uniform sampler2D e;
+uniform sampler2D f;
+uniform int id;
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+  vec4 o;
+
+  if (id == 0) o = texture(a, uv);
+  if (id == 1) o = texture(b, uv);
+  if (id == 2) o = texture(c, uv);
+  if (id == 3) o = texture(d, uv);
+  if (id == 4) o = texture(e, uv);
+  if (id == 5) o = texture(f, uv);
+
+  fragColor = o;
+}`;
+
 export const GLSL_PRESETS: Record<string, { type: string; data: { code: string } }> = {
 	'red.gl': { type: 'glsl', data: { code: RED_GL.trim() } },
 	'mix.gl': { type: 'glsl', data: { code: MIX_GL.trim() } },
@@ -72,5 +93,6 @@ export const GLSL_PRESETS: Record<string, { type: string; data: { code: string }
 	'null.gl': { type: 'glsl', data: { code: PASSTHRU_GL.trim() } },
 	'overlay.gl': { type: 'glsl', data: { code: OVERLAY_GL.trim() } },
 	'fft-freq.gl': { type: 'glsl', data: { code: AUDIO_FFT_FREQ_GL.trim() } },
-	'fft-waveform.gl': { type: 'glsl', data: { code: AUDIO_FFT_WAVEFORM_GL.trim() } }
+	'fft-waveform.gl': { type: 'glsl', data: { code: AUDIO_FFT_WAVEFORM_GL.trim() } },
+	'switcher.gl': { type: 'glsl', data: { code: SWITCHER_GL.trim() } }
 };
