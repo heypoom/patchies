e0d01ce86d11e89a6dca55a798e24e2a355849be
Phoomparin Mano <phoomparin@gmail.com>
2025-09-07 14:36:29 +0700

    preset: add filet mignon by afalfl

--- ui/src/lib/presets/hydra.presets.ts
+++ ui/src/lib/presets/hydra.presets.ts
@@ -42,6 +42,31 @@ shape(({time})=>Math.sin(time)+1*3, .5,.01)
 
 render(o0)`;
 
+const FILET_MIGNON = `// licensed with CC BY-NC-SA 4.0
+// https://creativecommons.org/licenses/by-nc-sa/4.0
+// filet mignon :: AFALFL :: instagram/a_f_alfl 
+
+osc(100, -0.0018, 0.17)
+  .diff(osc(20, 0.00008).rotate(Math.PI / 0.00003))
+  .modulateScale(
+    noise(1.5, 0.18).modulateScale(
+      osc(13).rotate(() => Math.sin(time / 22))
+    ), 3)
+  .color(11, 0.5, 0.4, 0.9, 0.2, 0.011, 5, 22, 0.5, -1)
+  .contrast(1.4)
+  .add(src(o0).modulate(o0, .04), .6, .9)
+  .invert()
+  .brightness(0.0003, 2)
+  .contrast(0.5, 2, 0.1, 2)
+  .color(4, -2, 0.1)
+  .modulateScale(osc(2), -0.2, 2, 1, 0.3)
+  .posterize(200)
+  .rotate(1, 0.2, 0.01, 0.001)
+  .color(22, -2, 0.5, 0.5, 0.0001, 0.1, 0.2, 8)
+  .contrast(0.18, 0.3, 0.1, 0.2, 0.03, 1)
+  .brightness(0.0001, -1, 10)
+  .out()`;
+
 type HydraNodeData = {
 	code: string;
 	messageInletCount?: number;
@@ -92,6 +117,10 @@ export const HYDRA_PRESETS: Record<string, { type: string; data: HydraNodeData }
 	'beans.hydra': {
 		type: 'hydra',
 		data: { ...defaultsOneVideoIn, code: BEANS.trim(), videoInletCount: 0 }
+	},
+	'filet-mignon.hydra': {
+		type: 'hydra',
+		data: { ...defaultsOneVideoIn, code: FILET_MIGNON.trim(), videoInletCount: 0 }
 	},
 	'fft.hydra': {
 		type: 'hydra',
