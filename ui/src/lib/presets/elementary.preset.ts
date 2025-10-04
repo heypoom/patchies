5c623acc8899c8aa0fc818a71150edeec1ff1c24
Phoomparin Mano <phoomparin@gmail.com>
2025-10-04 20:22:58 +0700

    presets: add train.elem and phasor.elem

--- ui/src/lib/presets/elementary.preset.ts
+++ ui/src/lib/presets/elementary.preset.ts
@@ -14,9 +14,27 @@ core.render(lpf, lpf)
 
 recv(freq => setCutoffFreq({value: freq}))`;
 
+const createRatePreset = (name: string) => `setPortCount(1)
+
+let [rate, setRate] = core.createRef("const", {
+  value: 440
+}, []);
+
+recv(freq => setRate({ value: freq }))
+
+core.render(el.${name}(rate), el.${name}(rate))`;
+
 export const ELEMENTARY_PRESETS = {
 	'lowpass.elem': {
 		type: 'elem~',
 		data: { code: LOWPASS_ELEM, messageInletCount: 1 }
+	},
+	'phasor.elem': {
+		type: 'elem~',
+		data: { code: createRatePreset('phasor'), messageInletCount: 1 }
+	},
+	'train.elem': {
+		type: 'elem~',
+		data: { code: createRatePreset('train'), messageInletCount: 1 }
 	}
 };
