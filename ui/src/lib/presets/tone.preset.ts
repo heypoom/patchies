274630c037d9c1fc398ac180ba93483ed7c77bc2
Phoomparin Mano <phoomparin@gmail.com>
2026-01-04 06:42:56 +0700

    presets: add reverb.tone and poly-synth-midi.tone

--- ui/src/lib/presets/tone.preset.ts
+++ ui/src/lib/presets/tone.preset.ts
@@ -1,4 +1,4 @@
-const POLY_SYNTH_JS = `setPortCount(1)
+const POLY_SYNTH_TRIGGER_JS = `setPortCount(1)
 
 const synth = new Tone.PolySynth(Tone.Synth)
 synth.connect(outputNode)
@@ -16,9 +16,69 @@ recv(message => {
 
 return { cleanup: () => synth.dispose() }`;
 
+const POLY_SYNTH_MIDI_JS = `setPortCount(1)
+
+const reverb = new Tone.Reverb({
+  decay: 2,
+  wet: 0.3
+}).connect(outputNode);
+
+const synth = new Tone.PolySynth(Tone.Synth, {
+  oscillator: {
+    type: "fatsine",
+    count: 1,
+    spread: 3
+  },
+  envelope: {
+    attack: 0.01,
+    release: 0.9
+  }
+}).connect(reverb);
+
+reverb.generate();
+
+recv(m => {
+  const now = Tone.now();
+
+  if (m.type === 'noteOn') {
+    const freq = Tone.Frequency(m.note, "midi").toNote();
+    const velocity = m.velocity / 127;
+    
+    synth.triggerAttack(freq, now, velocity);
+  } else if (m.type === 'noteOff') {
+    const freq = Tone.Frequency(m.note, "midi").toNote();
+    
+    synth.triggerRelease(freq, now);
+  } else if (m.type === 'pitchBend') {
+    synth.set({ detune: m.value * 200 });
+  }
+})
+
+return { 
+  cleanup: () => {
+    synth.dispose();
+    reverb.dispose();
+  } 
+}`;
+
 const PIPE_JS = `inputNode.connect(outputNode)
 
 return { cleanup: () => inputNode.disconnect(outputNode) }`;
+
+const REVERB_JS = `setPortCount(0)
+
+const reverb = new Tone.Reverb({
+  decay: 2,
+  wet: 0.3
+})
+
+inputNode.connect(reverb.input.input)
+reverb.connect(outputNode)
+reverb.generate()
+
+return { 
+  cleanup: () => reverb.dispose()
+}`;
 
 const LOWPASS_JS = `setPortCount(1)
 
@@ -33,13 +93,21 @@ recv(m => {
 return { cleanup: () => filter.dispose() }`;
 
 export const TONE_JS_PRESETS = {
-	'poly-synth.tone': {
+	'poly-synth-trigger.tone': {
 		type: 'tone~',
-		data: { code: POLY_SYNTH_JS, messageInletCount: 1 }
+		data: { code: POLY_SYNTH_TRIGGER_JS, messageInletCount: 1 }
+	},
+	'poly-synth-midi.tone': {
+		type: 'tone~',
+		data: { code: POLY_SYNTH_MIDI_JS, messageInletCount: 1 }
 	},
 	'pipe.tone': {
 		type: 'tone~',
 		data: { code: PIPE_JS, messageInletCount: 0 }
+	},
+	'reverb.tone': {
+		type: 'tone~',
+		data: { code: REVERB_JS, messageInletCount: 0 }
 	},
 	'lowpass.tone': {
 		type: 'tone~',
