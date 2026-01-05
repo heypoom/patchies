eae57637152ded7f26eb46d52af462089c904db5
Phoomparin Mano <phoomparin@gmail.com>
2026-01-05 21:53:14 +0700

    add startup modal compressed img

--- ui/src/lib/components/startup-modal/AboutTab.svelte
+++ ui/src/lib/components/startup-modal/AboutTab.svelte
@@ -2,7 +2,7 @@
 	import Icon from '@iconify/svelte';
 	import { onMount } from 'svelte';
 
-	import demoImage from '$lib/images/startup-modal.jpg';
+	import demoImage from '$lib/images/startup-modal-compressed.png';
 	import type { Tab } from './types';
 
 	let showOnStartup = $state(true);
