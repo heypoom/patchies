4788afe031d88e06ce42845c51b26fa1aba01cfc
Phoomparin Mano <phoomparin@gmail.com>
2026-01-08 03:51:21 +0700

    elem~ presets: take names

--- ui/src/lib/presets/elementary.preset.ts
+++ ui/src/lib/presets/elementary.preset.ts
@@ -1,4 +1,5 @@
 const LOWPASS_ELEM = `setPortCount(1)
+setTitle('lowpass~')
 
 const [cutoff, setCutoffFreq] = core.createRef("const", {
   value: 500
@@ -15,6 +16,7 @@ core.render(lpf, lpf)
 recv(freq => setCutoffFreq({value: freq}))`;
 
 const createRatePreset = (name: string) => `setPortCount(1)
+setTitle('${name}~')
 
 let [rate, setRate] = core.createRef("const", {
   value: 440
@@ -27,14 +29,14 @@ core.render(el.${name}(rate), el.${name}(rate))`;
 export const ELEMENTARY_PRESETS = {
 	'lowpass.elem': {
 		type: 'elem~',
-		data: { code: LOWPASS_ELEM, messageInletCount: 1 }
+		data: { code: LOWPASS_ELEM, messageInletCount: 1, title: 'lowpass~' }
 	},
 	'phasor.elem': {
 		type: 'elem~',
-		data: { code: createRatePreset('phasor'), messageInletCount: 1 }
+		data: { code: createRatePreset('phasor'), messageInletCount: 1, title: 'phasor~' }
 	},
 	'train.elem': {
 		type: 'elem~',
-		data: { code: createRatePreset('train'), messageInletCount: 1 }
+		data: { code: createRatePreset('train'), messageInletCount: 1, title: 'train~' }
 	}
 };
