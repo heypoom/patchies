0012ac53a10de0d16c1708ed021076f1e8115e49
Phoomparin Mano <phoomparin@gmail.com>
2026-01-18 17:20:55 +0700

    rendering: use 48fps cap for preview

--- ui/src/workers/rendering/constants.ts
+++ ui/src/workers/rendering/constants.ts
@@ -1,5 +1,5 @@
-/** Caps the preview at 40fps by default */
-export const DEFAULT_PREVIEW_MAX_FPS_CAP = 40;
+/** Caps the preview at this fps by default */
+export const DEFAULT_PREVIEW_MAX_FPS_CAP = 48;
 
 /** Caps the number of previews that can be rendered per frame when output is enabled */
 export const DEFAULT_MAX_PREVIEWS_PER_FRAME_WITH_OUTPUT = 20;
