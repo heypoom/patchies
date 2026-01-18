cc25a2cb988f9e1049d3d12d656e1c47f88a9549
Phoomparin Mano <phoomparin@gmail.com>
2026-01-18 16:59:10 +0700

    increase round-robin cap due to pbo

--- ui/src/workers/rendering/constants.ts
+++ ui/src/workers/rendering/constants.ts
@@ -2,7 +2,7 @@
 export const DEFAULT_PREVIEW_MAX_FPS_CAP = 40;
 
 /** Caps the number of previews that can be rendered per frame when output is enabled */
-export const DEFAULT_PREVIEW_MAX_PREVIEWS_PER_FRAME_WITH_OUTPUT = 4;
+export const DEFAULT_PREVIEW_MAX_PREVIEWS_PER_FRAME_WITH_OUTPUT = 20;
 
 /** Caps the number of previews that can be rendered per frame when output is disabled */
-export const DEFAULT_PREVIEW_MAX_PREVIEWS_PER_FRAME_NO_OUTPUT = 4;
+export const DEFAULT_PREVIEW_MAX_PREVIEWS_PER_FRAME_NO_OUTPUT = 20;
