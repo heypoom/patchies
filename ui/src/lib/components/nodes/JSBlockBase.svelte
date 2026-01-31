a4e484e4d0c20456a72bfd401f3942c3b26563c1
Phoomparin Mano <phoomparin@gmail.com>
2026-01-31 14:41:31 +0700

    worker: fix object title displaying as false

--- ui/src/lib/components/nodes/JSBlockBase.svelte
+++ ui/src/lib/components/nodes/JSBlockBase.svelte
@@ -197,7 +197,7 @@
       >
         <div class="z-10 w-fit rounded-lg bg-zinc-900 px-2 py-1 text-nowrap whitespace-nowrap">
           <div class="font-mono text-xs font-medium text-zinc-400">
-            {(supportsLibraries && data.libraryName) ?? data.title ?? nodeLabel}
+            {(supportsLibraries && data.libraryName) || data.title || nodeLabel}
           </div>
         </div>
 
