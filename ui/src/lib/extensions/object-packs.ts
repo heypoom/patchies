2addde8567c0abdc5f394503da35401e2a658047
Phoomparin Mano <phoomparin@gmail.com>
2026-02-03 21:23:27 +0800

    add f and i to object packs

--- ui/src/lib/extensions/object-packs.ts
+++ ui/src/lib/extensions/object-packs.ts
@@ -16,17 +16,7 @@ export const BUILT_IN_PACKS: ExtensionPack[] = [
     name: 'Control',
     description: 'When and where messages go',
     icon: 'GitBranch',
-    objects: [
-      'loadbang',
-      'metro',
-      'trigger',
-      'spigot',
-      'delay',
-      'throttle',
-      'debounce',
-      'float',
-      'int'
-    ]
+    objects: ['loadbang', 'metro', 'trigger', 'spigot', 'delay', 'throttle', 'debounce', 'f', 'i']
   },
   {
     id: 'transform',
