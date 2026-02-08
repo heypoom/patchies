c52f2ebf5b5c53aab7a83c7e71f88add7a81edda
Phoomparin Mano <phoomparin@gmail.com>
2026-02-09 03:46:21 +0800

    pack: move send.vdo and recv.vdo to media pack

--- ui/src/lib/extensions/object-packs.ts
+++ ui/src/lib/extensions/object-packs.ts
@@ -49,7 +49,18 @@ export const BUILT_IN_PACKS: ExtensionPack[] = [
     name: 'Media',
     description: 'Source and sinks for video and audio',
     icon: 'Camera',
-    objects: ['webcam', 'video', 'img', 'screen', 'mic~', 'soundfile~', 'out~', 'bg.out']
+    objects: [
+      'webcam',
+      'video',
+      'img',
+      'screen',
+      'mic~',
+      'soundfile~',
+      'out~',
+      'bg.out',
+      'send.vdo',
+      'recv.vdo'
+    ]
   },
   {
     id: '2d',
@@ -63,7 +74,7 @@ export const BUILT_IN_PACKS: ExtensionPack[] = [
     name: 'Video Synths',
     description: 'Hydra, shaders and 3D graphics',
     icon: 'Shapes',
-    objects: ['hydra', 'three', 'three.dom', 'glsl', 'swgl', 'send.vdo', 'recv.vdo']
+    objects: ['hydra', 'three', 'three.dom', 'glsl', 'swgl']
   },
   {
     id: 'audio',
