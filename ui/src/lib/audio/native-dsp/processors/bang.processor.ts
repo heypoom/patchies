a6f7c9cca35c99ff507d4cc3ffde53ab29512ecb
Phoomparin Mano <phoomparin@gmail.com>
2026-02-14 18:27:57 +0700

    bang~: send static bang

--- ui/src/lib/audio/native-dsp/processors/bang.processor.ts
+++ ui/src/lib/audio/native-dsp/processors/bang.processor.ts
@@ -1,13 +1,14 @@
 import { defineDSP } from '../define-dsp';
 
+const BANG = { type: 'bang' };
+
 defineDSP({
   name: 'bang~',
   audioInlets: 0,
   audioOutlets: 0,
-
   state: () => ({}),
 
   process(_state, _inputs, _outputs, send) {
-    send({ type: 'bang' }, 0);
+    send(BANG, 0);
   }
 });
